63191c537324b926ecb0ac15fd8761b640ca0e1a
MonPham <demondhunter93@gmail.com>
2023-02-19 15:17:59 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+CHAIN_ID=local-testnet 
+# KEY_NAME=wallet 
+MONIKER_NAME=${MONIKER_NAME:-local}
 dymd init "$MONIKER_NAME" --chain-id "$CHAIN_ID" 
-dymd keys add "$KEY_NAME" --keyring-backend test
+# dymd keys add "$KEY_NAME" --keyring-backend test
 #     dymd add-genesis-account "$(dymd keys show "$KEY_NAME" -a --keyring-backend test)" 100000000000stake && \
 #     dymd gentx "$KEY_NAME" 100000000stake --chain-id "$CHAIN_ID" --keyring-backend test && \
 #     dymd collect-gentxs
-CHAIN_ID=local-testnet 
-KEY_NAME=wallet 
-MONIKER_NAME=${MONIKER_NAME:-local}
+
 
 dymd version
 dymd help
